8afa831d3ed77087db6eac6947914f6dbd5fb2f5
Nikita <nikita-polishhuk@mail.ru>
2019-05-03 21:30:54 +0700

    Menu.c/h work done. main.c was started

---
+++ src/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "include/menu.h"
+
+int main()
+{
+    int choice;
+    intro();
+    choice = menu();
+    if (choice = 1) //Для training
+    {               //Example
+        //trainig();
+    }
+    if (choice = 2) //For main
+    {
+    }
+    if (choice = 3) //For competitive
+    {
+    }
+    if (choice = 4) //For rating
+    {
+    }
+    if (choice = 5) //For statistic
+    {
+    }
+
+    return 0;
+}
