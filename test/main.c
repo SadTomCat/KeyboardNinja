11e22a499d53f81131a87fb2d3cd353c066df4aa
Nikita <nikita-polishhuk@mail.ru>
2019-06-15 23:45:39 +0700

    test.h and main.c

--- test/main.c
+++ test/main.c
@@ -1,6 +1,12 @@
 #define CTEST_MAIN
 
 #include "../thirdparty/ctest.h"
+#include "../src/include/training.h"
+#include "../src/include/rating.h"
+#include "../src/include/scene.h"
+#include "../src/include/read_and_write.h"
+#include "../src/include/menu.h"
+
 int main(int argc, const char **argv)
 {
     return ctest_main(argc, argv);
