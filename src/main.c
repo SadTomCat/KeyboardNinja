1e41f991c9785dfd73c8d94b14573daa74b1e22b
eduardshindel <shindeleduard10@gmail.com>
2019-06-16 21:55:55 +0700

    Edit main.c

--- src/main.c
+++ src/main.c
@@ -11,16 +11,9 @@ int main()
     //Profile *profile = add_profile();
     Profile *profile = sing_in();
     printf("%s", profile->name);
-     
-    Profile *profile1 = malloc(sizeof(*profile1));
-    profile1->name = "jija_loh";
-    profile1->levels_passed = 9;
-    profile1->place_in_rating = 102;
-    profile1->point = 322;
-    profile1->interface = 1;
 
     //intro(profile1->interface);
-    menu(profile1, profile1->interface);
+    menu(profile, profile->interface);
 
     return 0;
 }
